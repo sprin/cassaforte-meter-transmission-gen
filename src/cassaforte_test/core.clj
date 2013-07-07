f8405f06bca24ae1dd3beadc20b49f9af911f70d
sprin <sprin>
2013-07-07 08:52:10 -0700

    Create initial script to generate meter samples and insert into cassandra

--- src/cassaforte_test/core.clj
+++ src/cassaforte_test/core.clj
@@ -1,9 +1,26 @@
 (ns cassaforte-test.core
-  (:gen-class))
+  (:require [clojurewerkz.cassaforte.client :as client])
+  (:use clojurewerkz.cassaforte.cql
+        clojurewerkz.cassaforte.query))
+
+
+;; Microseconds starting from 2013-01-01
+(def datetimes (iterate inc 1357027260000000))
+
+(defn timenow [] (new java.util.Date))
+
+;; Table creation:
+;; create table meter_samples
+;; (said int, datetime bigint, volts float, amps float,
+;; PRIMARY KEY (said, datetime));
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (client/connect! ["127.0.0.1"])
+  (use-keyspace "disagg")
+  (time (doseq [dt (take 10000 datetimes)]
+    (client/execute
+        ["INSERT INTO meter_samples (said, datetime, volts, amps)
+        VALUES (?, ?, ?, ?);"
+        [(int 1) (long dt) (float 120) (float 1)]]
+      :prepared true))))
