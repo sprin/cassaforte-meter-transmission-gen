8d268a7ccb85364d80bb196585e7fe54e3b0c0e2
sprin <sprin>
2013-07-07 12:28:43 -0700

    Add clj-time as dep

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clojurewerkz/cassaforte "1.0.0-rc5"]]
+                 [clojurewerkz/cassaforte "1.0.0-rc5"]
+                 [clj-time "0.5.1"]]
   :main cassaforte-test.core)
