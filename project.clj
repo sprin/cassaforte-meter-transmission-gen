ea05053af72ce03ef22bbd30b88bce689b2735ab
sprin <sprin>
2013-07-08 16:55:45 -0700

    Use latest Cassaforte snapshot

--- project.clj
+++ project.clj
@@ -1,12 +1,8 @@
 (defproject cassaforte-test "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A script to generate 15kHz power meter data and
+               insert/aggregate into a Cassandra cluster"
+  :url "https://github.com/sprin/cassaforte-meter-transmission-gen"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-time "0.5.1"]
-                 [cc.qbits/hayt "1.1.2"
-                  :exclusions [org.flatland/useful]]
-                 [com.datastax.cassandra/cassandra-driver-core "1.0.1"]]
-  :plugins [[lein-checkouts "1.1.0"]]
+                 [clojurewerkz/cassaforte "1.0.0-rc6-SNAPSHOT"]]
   :main cassaforte-test.core)
